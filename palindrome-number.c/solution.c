7aa064ea205ced05fe9dcc10f5580eecda31fa24
kevinnls <developer@kevinnlsamuel.com>
2025-07-24 23:25:20 +0530

    working thing?

--- palindrome-number.c/solution.c
+++ palindrome-number.c/solution.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdio.h>
 
 #include <math.h>
 
@@ -6,24 +7,20 @@ bool isPalindrome(int x) {
     if (x<0) return false;
 
     int ndigits = 1;
+	int powsof10[10] = {1,10};
 
-    while (x / (int) pow(10.0,ndigits) > 0)
+    while (x / powsof10[ndigits] > 0) {
         ndigits+=1;
+		powsof10[ndigits] = (int)pow(10.0,ndigits);
+	}
 
     if (ndigits==1) return true;
-    if (ndigits==2 && x%11==0) return true;
 
-    int evenNdigits = ndigits % 2 == 0;
-    int splitpoint = evenNdigits ? ndigits/2 : ndigits/2+1;
+	for (int i=1; i<=ndigits/2; i++){
+		int right = (x % powsof10[i]) / powsof10[i-1];
+		int left = (x % powsof10[ndigits-i+1]) / powsof10[ndigits-i];
+		if (left!=right) return false;
+	}
 
-    int left, right;
-
-    right = x % (int) pow(10.0,splitpoint);
-    left = x / (int) pow(10.0, evenNdigits ? splitpoint : splitpoint-1);
-
-    if (right == 0 || left == 0 || (right+left)/10==0) return false;
-
-    if ((left+right)%11==0) return true;
-
-    return false;
+    return true;
 }
